b8208a8ecb9ff414cb4efc8b6b4c871d0460072f
root <root@ip-172-31-14-190.ap-south-1.compute.internal>
2019-06-09 18:25:26 +0000

    2nd attempt

--- problem2.py
+++ problem2.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import time
 from googlesearch import search
-web=input("plz enter topic")
+web=input(" Plz enter topic-->> ")
+f=open("TopTenUrl.txt","w")
+for i in search(web,tld='com',lang='en',num=20,start=0,stop=10,pause=2.0):
+	f.write(i+"\n")
 
+#f=open("hello.txt",'w')
+#f.write(i+"\n")
 
-url=[]
 
